8ec4c78bf1cecf3e3a2370d89aaa5d63a98e7560
JKaypa <jose_kaypa@hotmail.com>
2023-07-26 12:18:52 -0500

    genres bulkCreate deleted

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,5 @@
 import app from "./app";
 import { sequelize } from "./dataBase/db";
-import { getGenres } from "./controllers/genres.controller";
 import config from "./config/config";
 const PORT = config.port;
 
@@ -8,7 +7,6 @@ const PORT = config.port;
 (async function () {
   try {
     await sequelize.sync({force: false});
-    await getGenres();
     app.listen(PORT)
     console.log('DataBase connected, server listening on port', PORT);
   } catch (error) {
