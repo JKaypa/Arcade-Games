91532d426e5e66420cc0ded3586147719b325710
JKaypa <jose_kaypa@hotmail.com>
2023-08-24 10:56:42 -0500

    delete alphabetical order option(made by default now)

--- front-end/src/components/home/Home.tsx
+++ front-end/src/components/home/Home.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import { useAppDispatch } from "../../hooks/redux.hooks";
-import { order } from "../../store/videogamesSlice";
 import Genres from "../genres/Genres";
 import Platforms from "../platforms/Platforms";
 import Videogames from "../videogames/Videogames";
@@ -9,17 +7,14 @@ import style from "./home.module.css";
 function Home() {
   const [genre, setGenre] = useState("");
   const [platform, setPlatform] = useState("");
-  const [alph, setAlph] = useState("");
   const [rating, setRating] = useState("");
 
-  const dispatch = useAppDispatch();
 
   const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const {name, value} = event.target
     if (name === "genres") setGenre(value)  
     else if (name === 'platforms') setPlatform(value)
-    else value && dispatch(order(value));
-
+    else setRating(value)
   };
 
   return (
@@ -34,23 +29,13 @@ function Home() {
           />
           <select
             className={style.buttons}
-            name="alph"
-            value={alph}
-            onChange={handleChange}
-          >
-            <option value="">Order alphabetically</option>
-            <option value="a-z">A -{">"} Z</option>
-            <option value="z-a">Z -{">"} A</option>
-          </select>
-          <select
-            className={style.buttons}
             name="rating"
             value={rating}
             onChange={handleChange}
           >
             <option value="">Order by rating</option>
-            <option value="ascending">Ascending</option>
-            <option value="descending">Descending</option>
+            <option value="ASC">Ascending</option>
+            <option value="DESC">Descending</option>
           </select>
         </div>
         <Videogames genre={genre} platform={platform}/>
--- front-end/src/store/videogamesSlice.ts
+++ front-end/src/store/videogamesSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { State, Options } from "../types";
+import { State } from "../types";
 import { allGames, gameById, createGame, updateGame, deleteGame } from "./actions";
 
 
@@ -28,19 +28,6 @@ export const videogamesSlice = createSlice({
     },
     restore: state => {
       state.message = ''
-    },
-    order: (state, action: PayloadAction<string>) => {
-      if(action.payload === 'ascending') 
-          state.videogames.sort((a, b) => parseInt(a.rating) - parseInt(b.rating))
-
-      else if(action.payload === 'descending')
-          state.videogames.sort((a, b) => parseInt(b.rating) - parseInt(a.rating))
-
-      else if(action.payload === 'a-z')
-          state.videogames.sort((a, b) => a.name > b.name ? 1 : -1)
-
-      else if(action.payload === 'z-a')
-          state.videogames.sort((a, b) => a.name < b.name ? 1 : -1)
     }
   },
   extraReducers: builder => {
@@ -68,5 +55,5 @@ export const videogamesSlice = createSlice({
 })
 
 
-export const { clean, name, restart, restore, order } = videogamesSlice.actions;
+export const { clean, name, restart, restore } = videogamesSlice.actions;
 export default videogamesSlice.reducer;
